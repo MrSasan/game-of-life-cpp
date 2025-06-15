eee60ce95751bbb0cd1cca8a8c4c27a0807e6579
Sasan <sasan.karim@mothworks.me>
2025-06-15 23:38:37 +0200

    added blank main.cpp

---
+++ main.cpp
@@ -0,0 +1,6 @@
+/*
+	Titel: Informatik Labor 6-7 - Conway's Game of Life
+	Beschreibung: A C++ implementation of Conway's Game of Life.
+	Name: Sasan Karim / Meliksah Unal / Daniel  
+	Datum: 16.06.2025
+*/
