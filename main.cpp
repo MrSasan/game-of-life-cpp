d4b8e663cb1f5e8e9f3e02cca6e16579fac4a86c
Sasan <17885066+MrSasan@users.noreply.github.com>
2025-06-16 13:57:01 +0200

    Added: - getConsoleSize function - Welcome screen

--- main.cpp
+++ main.cpp
@@ -4,3 +4,46 @@
 	Name: Sasan Karim / Meliksah Unal / Daniel  
 	Datum: 16.06.2025
 */
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+// Globale Variablen
+int gridRows = 0;
+int gridCols = 0;
+
+void getConsoleSize(int& row, int& cols) {
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+		row = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+		cols = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	}
+	else {
+		// Fallback value if unable to retrieve console size.
+		row = 30;
+		cols = 50;
+	}
+}
+
+
+
+int main() {
+	char menuSelect;
+	cout << "Welcome to Conway's Game of Life!" << endl
+		<< "Please resize your console window to the desired size, as this will be used to generate the grid for the game." << endl
+		<< "When you are ready, type 'R' or type 'Q' to quit the game." << endl;
+	cin >> menuSelect;
+	if (menuSelect == 'R' || menuSelect == 'r') {
+		getConsoleSize(gridRows, gridCols);
+		cout << "rows: " << gridRows << ", cols: " << gridCols << endl;
+	}
+	else if (menuSelect == 'Q' || menuSelect == 'q') {
+		cout << "Exiting the game. Goodbye!" << endl;
+		return 0;
+	}
+	else {
+		cout << "Invalid selection. Please restart the program." << endl;
+		return 1;
+	}
+}
