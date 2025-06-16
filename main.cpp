352fdbfe18e4eaaf7c6146f594495cf94ca9640c
Sasan <17885066+MrSasan@users.noreply.github.com>
2025-06-16 15:12:49 +0200

    Minor fix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
 	Titel: Informatik Labor 6-7 - Conway's Game of Life
 	Beschreibung: A C++ implementation of Conway's Game of Life.
-	Name: Sasan Karim / Meliksah Unal / Daniel  
+	Name: Sasan Karim / Meliksah Unal / Daniel Ochs
 	Datum: 16.06.2025
 */
 
@@ -12,10 +12,11 @@ using namespace std;
 // Globale Variablen
 int gridRows = 0;
 int gridCols = 0;
+int grid[999][999];
 
 void getConsoleSize(int& row, int& cols) {
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {                                
 		row = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 		cols = csbi.srWindow.Right - csbi.srWindow.Left + 1;
 	}
@@ -36,7 +37,8 @@ int main() {
 	cin >> menuSelect;
 	if (menuSelect == 'R' || menuSelect == 'r') {
 		getConsoleSize(gridRows, gridCols);
-		cout << "rows: " << gridRows << ", cols: " << gridCols << endl;
+		cout << "rows: " << gridRows << ", cols: " << gridCols << endl; //debug
+		// TODO
 	}
 	else if (menuSelect == 'Q' || menuSelect == 'q') {
 		cout << "Exiting the game. Goodbye!" << endl;
